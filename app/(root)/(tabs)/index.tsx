f15917891beb07de36f046f945fd991b6c6e7b09
Sahiljeet singh kalsi <sahiljeetsinghkalsi@gmail.com>
2025-03-03 12:26:25 +0530

    Refactor app configuration and enhance UI with splash screen and font integration

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,5 +1,5 @@
 import { Link } from "expo-router";
-import { Text, View } from "react-native";
+import { Text, View, Image } from "react-native";
 
 export default function Index() {
   return (
@@ -11,13 +11,18 @@ export default function Index() {
       }}
     >
       <Text className="font-bold text-lg my-10">Welcome chiggas</Text>
+      {/* Add the splash-icon image here */}
+      <Image
+        source={require("../../../assets/images/icon.png")}
+        style={{ width: 200, height: 200 }} // Adjust size as needed
+        resizeMode="contain" // Keeps the image proportionate
+      />
       <Link href={'/sign-in'}>Sign In</Link>
       <Link href={'/explore'}>Explore</Link>
       <Link href={'/profile'}>Profile</Link>
       <Link href={{ pathname: "/(root)/properties/[id]", params: { id: "123" } }}>
-  Property
-</Link>
-
+        Property
+      </Link>
     </View>
   );
 }
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,29 @@
-import { Stack } from "expo-router";
-import "./global.css";
+import { SplashScreen, Stack } from "expo-router";
+import { useFonts } from "expo-font";
+import { useEffect } from "react";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+  const [fontLoaded] = useFonts({
+    "Rubik-Regular": require("../assets/fonts/Rubik-Regular.ttf"),
+    "Rubik-Bold": require("../assets/fonts/Rubik-Bold.ttf"),
+    "Rubik-ExtraBold": require("../assets/fonts/Rubik-ExtraBold.ttf"),
+    "Rubik-Light": require("../assets/fonts/Rubik-Light.ttf"),
+    "Rubik-Medium": require("../assets/fonts/Rubik-Medium.ttf"),
+    "Rubik-SemiBold": require("../assets/fonts/Rubik-SemiBold.ttf"),
+  });
+
+  useEffect(() => {
+    console.log("Fonts loaded:", fontLoaded);
+    if (fontLoaded) {
+        SplashScreen.hideAsync();
+    }3000
+  }, [fontLoaded]);
+
+  if (!fontLoaded) {
+    return null;
+  }
+
   return <Stack />;
 }
