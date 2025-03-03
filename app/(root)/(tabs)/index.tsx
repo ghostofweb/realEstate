2bd1f573bb8e0ab20a04d415a7e39e8787b37f83
Sahiljeet singh kalsi <sahiljeetsinghkalsi@gmail.com>
2025-03-03 10:53:23 +0530

    Add initial project setup with Tailwind CSS integration and remove unused assets

---
+++ app/(root)/(tabs)/index.tsx
@@ -0,0 +1,23 @@
+import { Link } from "expo-router";
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text className="font-bold text-lg my-10">Welcome chiggas</Text>
+      <Link href={'/sign-in'}>Sign In</Link>
+      <Link href={'/explore'}>Explore</Link>
+      <Link href={'/profile'}>Profile</Link>
+      <Link href={{ pathname: "/(root)/properties/[id]", params: { id: "123" } }}>
+  Property
+</Link>
+
+    </View>
+  );
+}
